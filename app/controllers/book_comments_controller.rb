1b35e3a7e6bb132046aa6d08fa0fc49d4249a3b4
ryuseitaked <ryutake0306@gmail.com>
2021-04-04 22:05:17 +0000

    [finish}

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,17 +4,22 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    comment.save
+    if comment.save
+      flash[:success] = "Comment was successfully created."
+    else
     redirect_to book_path(book)
+    end
   end
 
   def destroy
+    BookComment.find_by(id:params[:id], book_id: params[:book_id]).destroy
+    redirect_to book_path(params[:book_id])
   end
   
   private
 
   def book_comment_params
-    params.require(:_comment).permit(:comment)
+    params.require(:book_comment).permit(:comment)
   end
 
 end
